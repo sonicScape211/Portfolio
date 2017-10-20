656e67c23713c8ad50c588def61c8bf6e04e73bd
JT <jmozingo12@wou.edu>
2017-10-20 13:16:47 -0700

    Added the StackADT interface and LinkedStack classes

--- 460hw/hw3/Calculator/Calculator/Program.cs
+++ 460hw/hw3/Calculator/Calculator/Program.cs
@@ -13,6 +13,100 @@ namespace Calculator
         }
     }
 
+    interface IStackADT
+    {
+        ///<summary>
+        ///This will push an item onto the top of the stack. This will return a reference
+        ///to the Object (pointer address, but not the copy of the item.) that was pushed so that
+        ///the anonymous object can be pushed to the stack and tehn used.
+        ///<paramref name="newItem"/> Will be the object pushed to the top of the stack.
+        ///<returns>A referance to the object that what pushed.</returns>
+        ///</summary>
+
+        Object Push(Object newItem);
+
+        Object Pop();
+
+        Object Peek();
+
+        Boolean isEmpty();
+
+        void clear();
+
+    }
+
+    //The LinkedStack class implements (:) the interface IStackADT.
+    class LinkedStack : IStackADT
+    {
+        //The top of the stack.
+        private Node top;
+
+        public LinkedStack()
+        {
+            top = null;
+        }
+
+        public Object Push(Object newItem)
+        {
+            //We cannot push a null node.
+            if (newItem == null)
+            {
+                //Reject.
+                return null;
+            }
+            //Create a new node and have it point to the current top-of-stack node.
+            Node newNode = new Node(newItem, top);
+            //Push to the top of the stack.
+            top = newNode;
+            //return the node jsut pushed.
+            return newItem;
+        }
+
+        public Object Pop()
+        {
+            //If the stack is empty we cant pop anything.
+            if (isEmpty())
+            {
+                ///Reject.
+                return null;
+            }
+            //Get the information from the top-of-stack node.
+            Object topItem = top.data;
+            //Change the var top pointer to the next element.
+            top = top.next;
+            //Return the pointer to the element that was poped off the stack.
+            return topItem;
+
+        }
+
+        public Object Peek()
+        {
+            if (isEmpty())
+            {
+                return null;
+            }
+            return top.data;
+        }
+
+        /// <summary>
+        /// Checks to see if there is a pointer stored in the 'top' variable.
+        /// </summary>
+        /// <returns>If null return false. Else true</returns>
+        public Boolean isEmpty()
+        {
+            return top == null;
+        }
+
+        /// <summary>
+        /// This will clear the entire stack by removing the pointer to the
+        /// top element on the stack.
+        /// </summary>
+        public void clear()
+        {
+            top = null;
+        }
+    }
+
     /// <summary>
     /// This class will be a singly linked node class.
     /// </summary>
