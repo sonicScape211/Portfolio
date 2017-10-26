26f1391a312480df4d65c6fe473d70c97a96273a
JT <jmozingo12@wou.edu>
2017-10-26 08:24:09 -0700

    Initial commit for the Temperature adjustment feature

---
+++ 460hw/hw4/Homework4/Homework4/Controllers/TemperatureAdjustmentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework4.Controllers
+{
+    public class TemperatureAdjustmentController : Controller
+    {
+        // GET: TemperatureAdjustment
+        public ActionResult TemperatureAdjustment()
+        {
+            return View();
+        }
+    }
+}
