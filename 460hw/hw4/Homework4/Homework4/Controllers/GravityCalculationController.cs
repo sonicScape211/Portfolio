c62698b8f0559ed1272ff16291e5e38e5b06b1a9
JT <jmozingo12@wou.edu>
2017-10-25 17:23:41 -0700

    Created the Gravity Calculation page along with the View-Controller functionality. There is now the ability to calculate the ABV of your homebrew.

--- 460hw/hw4/Homework4/Homework4/Controllers/GravityCalculationController.cs
+++ 460hw/hw4/Homework4/Homework4/Controllers/GravityCalculationController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,25 @@ namespace Homework4.Controllers
         public ActionResult GravityCalculation()
         {
             return View();
+            
         }
+
+        public ActionResult GravityFormInformation()
+        {
+            string originalGravity = Request.Form["original-gravity"];
+            string specificGravity = Request.Form["specific-gravity"];
+           
+            ViewBag.originalGravity = originalGravity;
+            ViewBag.specificGravity = specificGravity;
+
+            ViewData["abv"] = ((float.Parse(originalGravity) - float.Parse(specificGravity)) * 131).ToString(); 
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult GravityCalculation(string originalGravity)
+        //{
+          //  Debug.WriteLine($"{originalGravity}");
+           // return View();
+        //}
     }
 }
