06d60fa8d8393db8a976ff4602c4c0029b581aa9
JT <jmozingo12@wou.edu>
2017-10-26 14:32:38 -0700

    Initial commit for feature#3 - loan calculator

---
+++ 460hw/hw4/Homework4/Homework4/Controllers/LoanCalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework4.Controllers
+{
+    public class LoanCalculatorController : Controller
+    {
+        // GET: LoanCalculator
+        public ActionResult LoanCalculator()
+        {
+            return View();
+        }
+    }
+}
