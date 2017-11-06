dadb6d7ffe053acb2846421ad45dafc2ffe31f0c
JT <jmozingo12@wou.edu>
2017-11-06 13:25:48 -0800

    Updated XML Documentation and removed an unused parameter in the Scanner constructor.

--- 460hw/hw3/Calculator/Calculator/Program.cs
+++ 460hw/hw3/Calculator/Calculator/Program.cs
@@ -7,11 +7,16 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
+    /// <summary>
+    /// Calculator program using a postfix method. This calculator will implement a stack 
+    /// to store data including: the nubmers to be calculated and the operator to be used.
+    /// This program relies on the Scanner class within the Calculator namespace for processing
+    /// user input and returning usable data to the part of the program which performs arithmetic
+    /// operations.
+    /// </summary>
     class Calculator
     {
-        /**
-         * 
-         */
+      
         private IStackADT stack = new LinkedStack();
 
         static void Main(string[] args)
@@ -27,6 +32,10 @@ namespace Calculator
             Console.Write("Bye.");
         }
 
+        /// <summary>
+        /// Method to prompt the user for an appropriate input or to quit out of the program.
+        /// </summary>
+        /// <returns>Boolean If the user wants to keep going.</returns>
         private bool doCalculation()
         {
             Console.Write("Please enter q to quit\n");
@@ -58,7 +67,13 @@ namespace Calculator
             return true;
         }
 
-
+        /// <summary>
+        /// Logical method for evaluating the user input and parsing the input.
+        /// It then passes in the parsed elements to the operation method for 
+        /// the actual mathematical calculations. 
+        /// </summary>
+        /// <param name="input">String of the users input.</param>
+        /// <returns>String representation of the equation result.</returns>
         public String evaluatePostFixInput(String input)
         {
             if (input == null || input.Equals(""))
@@ -73,7 +88,7 @@ namespace Calculator
             double b;   //...for operand.
             double c;   //...for answer.
 
-            Scanner st = new Scanner(input, input.Length);
+            Scanner st = new Scanner(input);
 
             while (st.hasNext())
             {
@@ -109,6 +124,14 @@ namespace Calculator
             return ((double)stack.Pop()).ToString();
         }
 
+        /// <summary>
+        /// Evaluation of the user input and the mathematical functionality of
+        /// the program. Take the inputs and the operator and do the operation.
+        /// </summary>
+        /// <param name="a">Double First Number</param>
+        /// <param name="b">Double Second Number</param>
+        /// <param name="s">String Mathematical Operator</param>
+        /// <returns></returns>
         public double doOperation(double a, double b, String s)
         {
             double c = 0.0;
@@ -143,6 +166,10 @@ namespace Calculator
         }
 
     }
+
+    /// <summary>
+    /// Custom made Scanner class for parsing a String input.
+    /// </summary>
     class Scanner
     {
         private String[] userInput;
@@ -150,13 +177,22 @@ namespace Calculator
         private bool hasNextFlag = false;
         private bool hasNextDoubleFlag = false;
 
-        public Scanner(String input, int inputLength)
+        /// <summary>
+        /// Take the input and parse into substrings then store into String[] userInput.
+        /// </summary>
+        /// <param name="input">String User input.</param>
+        public Scanner(String input)
         {
 
             userInput = input.Split();
             //Pointer for where the scanner is at in the string.
             pointer = -1;
         }
+
+        /// <summary>
+        /// Method to check if there is another token in the user input.
+        /// </summary>
+        /// <returns>Boolean If there is another token availible.</returns>
 
         public bool hasNext()
         {
@@ -174,6 +210,11 @@ namespace Calculator
             }
         }
 
+        /// <summary>
+        /// Method to retrieve the next token in the string. Cannot be called without
+        /// checking for the presence of the next token with the hasNext method.
+        /// </summary>
+        /// <returns>String The token of the user input.</returns>
         public String next()
         {
             if (hasNextFlag)
@@ -188,6 +229,11 @@ namespace Calculator
             }
         }
 
+        /// <summary>
+        /// Method to check if there is another token in the user input and if that token is
+        /// a double.
+        /// </summary>
+        /// <returns>Boolean If there is another token availible.</returns>
         public bool hasNextDouble()
         {
             //Throw away var holder to satisfy the TryParse method.
@@ -206,6 +252,12 @@ namespace Calculator
             }
         }
 
+        /// <summary>
+        /// Method to retrieve the next double value of the token in the string. Cannot be 
+        /// called without checking for the presence of the next token with the 
+        /// hasNextDouble method.
+        /// </summary>
+        /// <returns>Double The token of the user input as a double.</returns>
         public double nextDouble()
         {
             if (hasNextDoubleFlag)
@@ -247,6 +299,13 @@ namespace Calculator
         void clear();
 
     }
+
+    ///<summary>
+    ///This is a class for a the structure of a stack of Node objects.
+    ///It will inculde all of the functionality of a stack; including
+    ///the link to the Top of the stack, and the interface methods for
+    ///push, pop, peek, isEmpty and clear.
+    ///</summary>
     //The LinkedStack class implements (:) the interface IStackADT.
     class LinkedStack : IStackADT
     {
@@ -258,6 +317,11 @@ namespace Calculator
             top = null;
         }
 
+        /// <summary>
+        /// "Push" a node object onto the stack and update the 'top' pointer.
+        /// </summary>
+        /// <param name="newItem"></param>
+        /// <returns>the item that was pushed to the top of the stack.</returns>
         public Object Push(Object newItem)
         {
             //We cannot push a null node.
@@ -274,6 +338,12 @@ namespace Calculator
             return newItem;
         }
 
+        /// <summary>
+        /// A method to remove the top Object from the stack and return that object.
+        /// It will also reassign the pointers for the top of the stack to the next node
+        /// in the stack.
+        /// </summary>
+        /// <returns>Object from the top of the stack.</returns>
         public Object Pop()
         {
             //If the stack is empty we cant pop anything.
@@ -291,6 +361,10 @@ namespace Calculator
 
         }
 
+        /// <summary>
+        /// Method to retrieve the data in the top Node Object.
+        /// </summary>
+        /// <returns>The data held by the top node.</returns>
         public Object Peek()
         {
             if (isEmpty())
@@ -321,26 +395,31 @@ namespace Calculator
 
 
     /// <summary>
-    /// This class will be a singly linked node class.
+    /// This class will be a singily linked node class capable of holding some 
+    /// data Object and a pointer to its neighbor node.
     /// </summary>
     class Node
     {
         public Object data; //The data within the current node.
         public Node next;   //The pointer to the node which is next in line.
 
-        /*
-         *This default constructor will create a new blank node with no data 
-         * or pointer to other nodes.
-         */
+        /// <summary>
+        /// This default constructor will create a new blank node with no data 
+        /// or pointer to other nodes.
+        /// </summary>
+        
         public Node()
         {
             data = null;
             next = null;
         }
-        /*
-         * This nondefault constructor will create a node with data and a pointer
-         * to the next node in line.
-         */
+
+        /// <summary>
+        /// This nondefault constructor will create a node with data and a pointer
+        ///  to the next node in line.
+        /// </summary>
+        /// <param name="data">Object Data to be held by the node.</param>
+        /// <param name="next">Node Neighbor node.</param>
         public Node(Object data, Node next)
         {
             this.data = data;
