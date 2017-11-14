ff80eb26888d416c89c3da68ca2d9e18d5532915
JT <jmozingo12@wou.edu>
2017-11-13 18:10:06 -0800

    Created the base landing page.

--- 460hw/hw6/Lab6/Lab6/App_Start/BundleConfig.cs
+++ 460hw/hw6/Lab6/Lab6/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Lab6
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap*",
                       "~/Content/site.css"));
         }
     }
