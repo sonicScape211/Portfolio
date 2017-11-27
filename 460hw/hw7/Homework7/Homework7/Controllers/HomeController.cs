b741c78146b3a6e3db7e97a518885041918fec69
JT <jmozingo12@wou.edu>
2017-11-27 14:29:22 -0800

    Initial commit for homework folders 7 and 8

---
+++ 460hw/hw7/Homework7/Homework7/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Web.Mvc;
+//Imported for use with the WebRequest class.
+using System.Net;
+//Imported for use with the reader and stream classes.
+using System.IO;
+//Imported for use with the JSON class.
+using System.Web.Helpers;
+//Imported for use with converting strings to JSON objects.
+using Newtonsoft.Json.Linq;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using Homework7.Models;
+
+namespace Homework7.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        /*
+         * Much of the following code is references from: https://docs.microsoft.com/en-us/dotnet/framework/network-programming/how-to-request-data-using-the-webrequest-class
+         * 
+         */
+        
+        public void Search(string searchInput) {
+
+            //Request encrypted api key from external source.
+            //NOTE this needs to be here because this key will not stick around in a field
+            //and for that matter shouldn't be stored away like that.
+            string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyAPIKey"];
+
+            string userQuery = searchInput;
+            string host = "api.giphy.com";
+            string path = "/v1/gifs/search";
+            
+            //Create the WebRequest instance by calling Create method.
+            WebRequest request = WebRequest.Create("https://"+ host + path + "?q=" + searchInput + "&api_key="+ apiKey + "&limit=1");
+            //Get the response from the API
+            WebResponse response = request.GetResponse();
+            //Get the stream of data from the server.
+            Stream dataStream = response.GetResponseStream();
+            //Create a reader for the data stream
+            //StreamReader reader = new StreamReader(dataStream).ReadToEnd();
+            //Read it!
+            string apiResponse = new StreamReader(dataStream).ReadToEnd();//reader.ReadToEnd();
+            //Debug.WriteLine(apiResponse);
+            //Convert that data stream string, which is already in a JSON format, to a json object
+            //JObject jsonResponse = JObject.Parse(apiResponse);
+            //Convert the JSON string to a .NET object 
+            //var result = JsonConvert.DeserializeObject(apiResponse);
+            //Debug.WriteLine(result);
+            var serializer = new JavaScriptSerializer();
+
+            var data = GiphyData.FromJson(apiResponse);
+
+            //var data = serializer.DeserializeObject(apiResponse);//reader.ReadToEnd()); 
+            Debug.WriteLine(apiResponse);
+            Debug.WriteLine(data);
+            //Close everything up
+            response.Close();
+            dataStream.Close();
+
+
+
+            //return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 460hw/hw7/Homework7/Homework7/Scripts/homepage.js
@@ -0,0 +1,32 @@
+﻿$("#search-button").on("click", function () {
+    alert("here");
+
+    search();
+
+});
+
+function search() {
+    //Get the value out of the text element
+    var searchInput = document.getElementById('search-input').value.trim();
+    
+    $.ajax({
+        type: "GET",
+        url: "/Home/Search",
+        //type of data expected back from the server. .ajax will by default try and infer but we will just specify here.
+        //dataType: "json",
+        //Data to be passed to GET function.
+        data: { searchInput: searchInput },
+
+        success: function () {
+            alert("Whoop");
+            console.log(data);
+            //alert(response);
+        },
+
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+}
