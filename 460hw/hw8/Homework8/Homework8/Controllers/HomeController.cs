eafa7ce0312ebb2943ebaeb96e619309c7fd9812
JT <jmozingo12@wou.edu>
2017-11-29 00:53:30 -0800

    Added ViewModel for homepage(genres/artworks), genres display currently

--- 460hw/hw8/Homework8/Homework8/Controllers/HomeController.cs
+++ 460hw/hw8/Homework8/Homework8/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Homework8.Models.ViewModels;
 
 namespace Homework8.Controllers
 {
     public class HomeController : Controller
     {
+        private ArtDbContext dbContext = new ArtDbContext();
+        //Create the view model to be passed into the homepage view on first opening the website.
+        public GenreArtworkViewModel genreArtwork = new GenreArtworkViewModel();
+
         public ActionResult Index()
         {
-            return View();
+            GenreArtworkViewModel completeViewModel = createViewModel(genreArtwork);
+            return View(completeViewModel);
+        }
+
+        private GenreArtworkViewModel createViewModel(GenreArtworkViewModel viewModel) {
+
+            viewModel.Artist = dbContext.Artists.ToList();
+            viewModel.Genre = dbContext.Genres.ToList();
+
+            return viewModel;
         }
 
         public ActionResult About()
---
+++ 460hw/hw8/Homework8/Homework8/Models/ViewModels/GenreArtworkViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework8.Models.ViewModels
+{
+    public class GenreArtworkViewModel
+    {
+        public IEnumerable<Artist> Artist { get; set; }
+        public IEnumerable<Genre> Genre { get; set; }
+
+    }
+}
